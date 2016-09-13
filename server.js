95898613b46e77874d34f3760adb64b99f3bea18
Samule1 <ham_car@hotmail.com>
2016-09-13 11:27:50 +0200

    Setup stuff

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-users = [];
-connections = [];
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  res.render('pages/index');
+});
+
+app.get('/game', function(req, res){
+  res.render('pages/game');
+})
 
 server.listen(process.env.PORT || 3000);
 console.log('Server running..');
