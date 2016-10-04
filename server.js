441173ffc39d656dba140d780e46eb58628f0af4
Flarpz <Marcuskarrm@gmail.com>
2016-10-04 10:08:41 +0200

    Fix hash for server side

--- server.js
+++ server.js
@@ -15,100 +15,126 @@ app.set('views', __dirname + '/Views');
 //app.use(express.static(__dirname+ '/Scripts'));
 app.use(express.static('public'));
 //Routes
-app.get('/', function(req, res){
-  res.render('pages/index');
+app.get('/', function (req, res) {
+    res.render('pages/index');
 });
 
-app.get('/game', function(req, res){
-  let uniqueId = helper.getRandomId5();
-  res.render('pages/game', {data: uniqueId});
+app.get('/game', function (req, res) {
+    let uniqueId = helper.getRandomId5();
+    res.render('pages/game', {data: uniqueId});
 });
 
-app.get('/connectionTest', function(req, res){
-  res.render('pages/connectionTest');
+app.get('/connectionTest', function (req, res) {
+    res.render('pages/connectionTest');
 });
 
-app.get('/testGame', function(req, res){
-  res.render('pages/pingpong');
+app.get('/testGame', function (req, res) {
+    res.render('pages/pingpong');
 });
-app.get('/gameframe', function(req, res){
-  res.render('partials/gameWindow');
+app.get('/gameframe', function (req, res) {
+    res.render('partials/gameWindow');
 });
-app.get('/inputCard', function(req, res){
-  res.render('partials/inputCard', {roomId: req.query.roomId, nick: req.query.nick})
+app.get('/inputCard', function (req, res) {
+    res.render('partials/inputCard', {roomId: req.query.roomId, nick: req.query.nick})
 });
 
-app.get('/stage', function(req, res){
-  let uniqueId = helper.getRandomId5();
-  let qrSource = "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=[roomId]"
-  let playerLimit = 2;
-  qrSource = qrSource.replace("[roomId]", uniqueId);
-  res.render('pages/stage', {data: uniqueId, qr: qrSource, playerLimit: playerLimit});
+app.get('/stage', function (req, res) {
+    let uniqueId = helper.getRandomId5();
+    let qrSource = "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=[roomId]"
+    let playerLimit = 2;
+    qrSource = qrSource.replace("[roomId]", uniqueId);
+    res.render('pages/stage', {data: uniqueId, qr: qrSource, playerLimit: playerLimit});
 });
 
 server.listen(process.env.PORT || 3000);
 console.log('Server running..');
-console.log('Listening on: '+ process.env.PORT);
+console.log('Listening on: ' + process.env.PORT);
 
 
 //Socket events..
 io.sockets.on('connection', (socket) => {
-  connections.push(socket);
-  console.log('Connected: %s sockets connected', connections.length);
+    let id = helper.getRandomId5();
+    while (connections[id] != undefined) {
+        id = helper.getRandomId5();
+    }
+    socket.id = id;
+    connections[socket.id] = socket;
+    connections.length++;
+    console.log('Connected: %s sockets connected', connections.length);
 
-  //Disconnect
-  socket.on('disconnect', (data)=>{
-    connections.splice(connections.indexOf(socket),1);
-    console.log('Disconnected: %s sockets connected', connections.length);
-  });
 
-  //Send message
-  socket.on('send message', (data)=>{
-    console.log(data);
-    activeGames[data.id].viewSockets.forEach(function(socket){
-      socket.emit('new message', {msg: data.msg, nick: data.nick});
+    //Disconnect
+    socket.on('disconnect', (data)=> {
+        if(socket.type === 'output'){
+            activeGames[socket.roomId].viewSockets.splice(socket.id, 1);
+            //generate disconnect event
+        }
+        else if(socket.type === 'input'){
+            activeGames[socket.roomId].inputSockets.splice(socket.id, 1);
+        }
+        else{
+            console.log('Error in disconnect' + socket);
+        }
+        connections.splice(connections[socket.id], 1)
+        console.log('Disconnected: %s sockets connected', connections.length);
     });
-  });
 
-  //Register a new socket
-  socket.on('register', (data)=>{
-    
-    //Checking if he socket is a browser or phone
-    if(data.type === 'output'){
+    //Send message
+    socket.on('send message', (data)=> {
+        console.log(data);
+        for(var viewSocketId in activeGames[data.id].viewSockets){
+            socket.emit('new message', {msg: data.msg, nick: data.nick});
+        }
+        /*
+        activeGames[data.id].viewSockets.forEach(function (socket) {
+            socket.emit('new message', {msg: data.msg, nick: data.nick});
+        });
+        */
+    });
 
-      //Creating a new Game-object
-      let game = new models.Game();
-      game.viewSockets.push(socket);
+    //Register a new socket
+    socket.on('register', (data)=> {
 
-      //Adding the instance
-      activeGames[data.id] = game;
+        //Checking if he socket is a browser or phone
+        if (data.type === 'output') {
 
-      console.log('Registred room: ' + data.id);
+            //Creating a new Game-object
+            let game = new models.Game();
+            game.viewSockets[socket.id] = socket.id;
 
-      //Here maybe an instance of the game should be created..
-    }
+            //Adding the instance
+            activeGames[data.id] = game;
+            socket.roomId = data.id;
+            socket.type = 'output';
 
-    else if(data.type === 'input'){
-      if(activeGames[data.id] === undefined){
-          socket.emit('new message', {msg: 'No room found', success: false});
-          socket.disconnect();
-          //disconnect the socket here..
-      }
-      else{
-          activeGames[data.id].inputSockets.push(socket);
-          activeGames[data.id].viewSockets.forEach((socket)=>{
-           socket.emit('new player connected', data);
-         });
-          socket.emit('new message', {msg: 'You are connected as input!', success: true});
+            console.log('Registred room: ' + data.id);
 
-          console.log('Registred new input to room: ' + data.id);
-      }
-    }
-  });
+            //Here maybe an instance of the game should be created..
+        }
 
-  //Disconnect input
-  socket.on('quit session', (data)=>{
+        else if (data.type === 'input') {
+            if (activeGames[data.id] === undefined) {
+                socket.emit('new message', {msg: 'No room found', success: false});
+                socket.disconnect();
+                //disconnect the socket here..
+            }
+            else {
+                activeGames[data.id].inputSockets[socket.id] = socket.id;
+                socket.roomId = data.id;
+                socket.type = 'input';
+                for(var viewSocketId in activeGames[data.id].viewSockets){
+                    connections[viewSocketId].emit('new player connected', data);
+                }
+                socket.emit('new message', {msg: 'You are connected as input!', success: true});
 
-  })
+                console.log('Registred new input to room: ' + data.id);
+            }
+        }
+    });
+
+    //Disconnect input
+    socket.on('quit session', (data)=> {
+
+    })
 
 });
